617599f774da48f4ae792b80b2f5b1c80436c77c
Renato Vásquez <rena_vasquez@icloud.com>
2022-12-12 23:17:24 -0300

    Add files via upload

---
+++ band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Generator")
+
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("Your band ame could be " + "'" + city_name + " " + pet_name + "'")
